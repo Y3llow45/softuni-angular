917441dc100b0ec768a0b51d34a16f6003b3189c
Borislav Hristov <bofengejn@gmail.com>
2023-09-07 10:23:18 +0300

    some angular

--- week1.2/test/src/app/app.component.ts
+++ week1.2/test/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test';
+  title = 'Test App Title';
+  badCurly = 'my-class';
+  visible = false;
+  users = [
+    {
+      name: 'User1',
+      age: 10
+    },
+    {
+      name: 'User2',
+      age: 22
+    },
+    {
+      name: 'User3',
+      age: 12
+    },
+  ];
+  toggleVisible() {
+    this.visible = !this.visible;
+  }
+  setTitle(newTitle:string) {
+    this.title = newTitle;
+  }
 }
