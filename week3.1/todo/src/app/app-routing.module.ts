99986a0f06b7ee6b1f311509e593ce357360bb7a
Borislav Hristov <bofengejn@gmail.com>
2023-11-20 20:45:37 +0200

    Add modules and components

--- week3.1/todo/src/app/app-routing.module.ts
+++ week3.1/todo/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent as UserListComponent} from './user/list/list.component';
+import { ListComponent as TodoListComponent } from './todo/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoListComponent
+  },
+  {
+    path: 'user',
+    component: UserListComponent
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
--- week3.1/todo/src/app/app.module.ts
+++ week3.1/todo/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
+    UserModule,
+    TodoModule,
     AppRoutingModule
   ],
   providers: [],
