260b6b73a94176f4c0c2124ea3e55fac05fd6420
Borislav Hristov <bofengejn@gmail.com>
2023-12-11 19:53:07 +0200

    Login

--- test-app-forms/src/app/app.module.ts
+++ test-app-forms/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    HighlightDirective,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- test-app-forms/src/app/highlight.directive.ts
+++ test-app-forms/src/app/highlight.directive.ts
@@ -1,15 +1,36 @@
-import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, Output, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnChanges {
-  constructor() { }
+  constructor(public renderer: Renderer2, public elementRef: ElementRef) { 
+    console.log(elementRef);
+  }
 
   @Input() value!: number;
   @Input() index!: number;
 
+  @HostListener('mouseover', ['$event']) mouseoverHandler(event: any) {
+    this.highlight('yellow')
+    this.textColor = 'red';
+  }
+
+  @HostListener('mouseleave', ['$event']) mouseleaveHandler(event: any) {
+    this.highlight('')
+    this.textColor = '';
+  }
+
+  @HostBinding('style.color') textColor = 'black';
+
   ngOnChanges(simpleChanges: SimpleChanges) {
-    console.log(simpleChanges)
+    this.highlight(this.value !== this.index ? '' : 'red');
+  }
+
+  highlight(color: string) {
+    
+    //this.elementRef.nativeElement.style.backgroundColor = 'red';
+
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', color);
   }
 }
