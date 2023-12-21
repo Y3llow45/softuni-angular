e487d0dd2e5c131bb2b6f383aec213fc3c248f01
Borislav Hristov <bofengejn@gmail.com>
2023-12-21 21:00:54 +0200

    pipe

--- week5.1/app/src/app/app.component.ts
+++ week5.1/app/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
+import { Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  posts$!: Observable<any[]>
+
+  constructor(private postService: PostService){}
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.loadPosts().pipe(
+      shareReplay(1)
+    );
+  }
 }
